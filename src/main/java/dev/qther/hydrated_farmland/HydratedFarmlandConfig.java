4baa93f50de99db64d718fb608426e069466060e
Qther <qther@tuta.io>
2026-01-21 09:35:29 +0800

    feat: change default ticks to 64

--- src/main/java/dev/qther/hydrated_farmland/HydratedFarmlandConfig.java
+++ src/main/java/dev/qther/hydrated_farmland/HydratedFarmlandConfig.java
@@ -6,7 +6,7 @@ import com.hypixel.hytale.codec.builder.BuilderCodec;
 
 public class HydratedFarmlandConfig {
     public int distance = 4;
-    public int ticks = 32;
+    public int ticks = 64;
 
     public static final BuilderCodec<HydratedFarmlandConfig> CODEC = BuilderCodec.builder(HydratedFarmlandConfig.class, HydratedFarmlandConfig::new)
             .append(new KeyedCodec<>("Distance", Codec.INTEGER), (data, value) -> data.distance = value, data -> data.distance).add()
